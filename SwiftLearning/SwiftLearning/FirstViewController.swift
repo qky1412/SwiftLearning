a26078cd28ae4e80ee842045839ba113fa3ac3be
qky1412 <qky1412@gmail.com>
2016-10-29 09:45:01 +0800

    init project

---
+++ SwiftLearning/SwiftLearning/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftLearning
+//
+//  Created by qky on 16/10/29.
+//  Copyright © 2016年 boostfield. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
